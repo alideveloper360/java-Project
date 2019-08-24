3abeffccc5a7d6542d3978441b295599af9afd58
alideveloper360 <54466947+alideveloper360@users.noreply.github.com>
2019-08-24 00:45:05 -0700

    Add files via upload

---
+++ src/JOptionPane.java
@@ -0,0 +1,29 @@
+
+import java.awt.print.PrinterException;
+import javax.swing.JRootPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Ali
+ */
+class JOptionPane {
+
+    static void showMessageDialog(JRootPane rootPane, String sucessfully_enter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String showInputDialoge(String family_Name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void showMessageDialog(JRootPane rootPane, PrinterException e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
